211d65f53772fe50b822e8fa57435178c6372f00
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-03-12 23:29:49 -0500

    POE rewrite

--- week2/server.pl
+++ week2/server.pl
@@ -3,8 +3,9 @@ use strict;
 use warnings;
 use 5.010;
 
-use Socket;
-use Fcntl;
+use POE;
+use POE::Wheel::UDP;
+use POE::Filter::Stream;
 use Getopt::Long;
 
 sub main {
@@ -18,93 +19,37 @@ sub main {
         'port=i'  => \$port,
     );
 
-    my $sock2 = sock2( $addr2, $port );
+    POE::Session->create(
+        inline_states => {
+            _start => sub {
+                $_[HEAP]->{wheel0} = POE::Wheel::UDP->new(
+                    LocalAddr  => $addr,
+                    LocalPort  => $port,
+                    InputEvent => 'input',
+                    Filter     => POE::Filter::Stream->new(),
+                );
+                $_[HEAP]->{wheel1} = POE::Wheel::UDP->new(
+                    LocalAddr  => $addr2,
+                    LocalPort  => $port,
+                    InputEvent => 'input',
+                    Filter     => POE::Filter::Stream->new(),
+                );
+            },
+            input => sub {
+                my $input = $_[ARG0];
+                say $input->{addr}, ':', $input->{port};
+                $_[HEAP]->{wheel1}->put(
+                    {   payload => ['Test'],
+                        addr    => $input->{addr},
+                        port    => $input->{port},
+                    }
+                );
+            },
+        },
+    );
 
-    my $proto = getprotobyname("udp");
-
-    my $sock;
-    socket( $sock, PF_INET, SOCK_DGRAM, $proto )
-        or die("socket() failure: $!");
-
-    fcntl( $sock, F_SETFL, O_NONBLOCK | O_RDWR )
-        or die("fcntl problem: $!");
-
-    setsockopt( $sock, SOL_SOCKET, SO_REUSEADDR, 1 )
-        or die("setsockopt SO_REUSEADDR failed: $!");
-
-    {
-        my $addr = inet_aton($addr)
-            or die("inet_aton problem: $!");
-        my $sockaddr = sockaddr_in( $port, $addr )
-            or die("sockaddr_in problem: $!");
-        bind( $sock, $sockaddr )
-            or die("bind error: $!");
-    }
-
-    my $socket = $sock;
-
-    $! = undef;
-    while (1) {
-        while ( my $a = recv( $socket, my $input = "", 1500, MSG_DONTWAIT ) )
-        {
-            if ( defined($addr) ) {
-                my %input_data;
-
-                if ($a) {
-                    my ( $port, $addr ) = unpack_sockaddr_in($a)
-                        or warn("sockaddr_in failure: $!");
-                    $input_data{addr} = inet_ntoa($addr);
-                    $input_data{port} = $port;
-
-                    my $ip = join( '.', unpack( 'CCCC', $addr ) );
-                    say "$ip:$port";
-                    say $input;
-                    respond( $sock2, $ip, $port );
-                }
-            }
-        }
-    }
-}
-
-sub respond {
-    my ( $sock, $ip, $port ) = @_;
-
-    #my $addr = inet_aton($a)
-    #or die("inet_aton problem: $!");
-    my $sockaddr = sockaddr_in( $port, pack( 'CCCC', split( /\./, $ip ) ) )
-        or die("sockaddr_in problem: $!");
-
-    #connect( $sock, $sockaddr )
-    #or die("connect error: $!");
-
-    send( $sock, 'Test', 0, $sockaddr );
-}
-
-sub sock2 {
-    my ( $addr, $port ) = @_;
-
-    my $proto = getprotobyname("udp");
-
-    my $sock;
-    socket( $sock, PF_INET, SOCK_DGRAM, $proto )
-        or die("socket() failure: $!");
-
-    fcntl( $sock, F_SETFL, O_NONBLOCK | O_RDWR )
-        or die("fcntl problem: $!");
-
-    setsockopt( $sock, SOL_SOCKET, SO_REUSEADDR, 1 )
-        or die("setsockopt SO_REUSEADDR failed: $!");
-
-    {
-        my $addr = inet_aton($addr)
-            or die("inet_aton problem: $!");
-        my $sockaddr = sockaddr_in( $port, $addr )
-            or die("sockaddr_in problem: $!");
-        bind( $sock, $sockaddr )
-            or die("bind error: $!");
-    }
-
-    return $sock;
+    POE::Kernel->run;
+    exit;
 }
 
 main(@ARGV);
