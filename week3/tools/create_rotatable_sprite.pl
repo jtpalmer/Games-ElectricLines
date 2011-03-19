9eab6b7b3e745109f4b9867588655e88c193f6ba
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-03-19 18:30:46 -0400

    Tool to create rotatable sprites

---
+++ week3/tools/create_rotatable_sprite.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Imager;
+
+sub main {
+    my ( $i_file, $o_file, $num ) = @_;
+
+    die "Need an input file\n"  unless $i_file;
+    die "Need an output file\n" unless $o_file;
+
+    $num //= 360 / 5;
+    my $deg = 360 / $num;
+
+    my $input = Imager->new;
+    $input->read( file => $i_file )
+        or die "Could not read: $i_file: " . $input->errstr;
+
+    my $size = size( $input->getwidth, $input->getheight );
+
+    my $output = Imager->new( xsize => $size * $num, ysize => $size );
+
+    foreach my $i ( 0 .. $num - 1 ) {
+        my $rot = $input->rotate( degrees => -$i * $deg );
+        my $left = $i * $size + ( $size - $rot->getwidth ) / 2;
+        my $top = ( $size - $rot->getheight ) / 2;
+        $output->paste( src => $rot, left => $left, top => $top );
+    }
+
+    $output->write( file => $o_file )
+        or die "Cannot save $o_file: ", $output->errstr;
+
+    exit;
+}
+
+sub size {
+    my ( $w, $h ) = @_;
+    return int( sqrt( $w * $w + $h * $h ) ) + 1;
+}
+
+main(@ARGV);
