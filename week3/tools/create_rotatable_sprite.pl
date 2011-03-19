728fd8015cc43bce999156ce89c9c37df3ad2111
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-03-19 18:59:44 -0400

    Add background color to car

--- week3/tools/create_rotatable_sprite.pl
+++ week3/tools/create_rotatable_sprite.pl
@@ -5,10 +5,12 @@ use 5.010;
 use Imager;
 
 sub main {
-    my ( $i_file, $o_file, $num ) = @_;
+    my ( $i_file, $o_file, $bg_color, $num ) = @_;
 
     die "Need an input file\n"  unless $i_file;
     die "Need an output file\n" unless $o_file;
+
+    $bg_color //= 'white';
 
     $num //= 360 / 5;
     my $deg = 360 / $num;
@@ -19,10 +21,19 @@ sub main {
 
     my $size = size( $input->getwidth, $input->getheight );
 
-    my $output = Imager->new( xsize => $size * $num, ysize => $size );
+    my $output
+        = Imager->new( xsize => $size * $num, ysize => $size, channels => 4 );
+    $output->box(
+        xmin   => 0,
+        ymin   => 0,
+        xmax   => $size * $num,
+        ymax   => $size,
+        filled => 1,
+        color  => $bg_color,
+    );
 
     foreach my $i ( 0 .. $num - 1 ) {
-        my $rot = $input->rotate( degrees => -$i * $deg );
+        my $rot = $input->rotate( degrees => -$i * $deg, back => $bg_color );
         my $left = $i * $size + ( $size - $rot->getwidth ) / 2;
         my $top = ( $size - $rot->getheight ) / 2;
         $output->paste( src => $rot, left => $left, top => $top );
