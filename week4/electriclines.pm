485d795d2b12c11cafb3b2ae77b5f0bc072e6d94
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-03-28 20:47:49 -0400

    Basic sanity checks

--- week4/electriclines.pm
+++ week4/electriclines.pm
@@ -6,13 +6,17 @@ use FindBin qw( $Bin );
 use lib "$Bin/lib";
 use Games::ElectricLines;
 
-my $lines = 4;
-my $exits = 1;
+my $lines  = 4;
+my $exits  = 1;
 my $result = GetOptions(
     'lines=i' => \$lines,
     'exits=i' => \$exits,
 );
 die "See README for usage\n" unless $result;
+
+$lines = 2          if $lines < 2;
+$exits = $lines - 1 if $exits >= $lines;
+$exits = 1          if $exits < 1;
 
 Games::ElectricLines->new(
     _row_count  => $lines,
