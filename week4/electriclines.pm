2aeb0ac3a878cf997d142d40e994526d1477070b
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-03-28 20:26:47 -0400

    Make number of exits and rows options

--- week4/electriclines.pm
+++ week4/electriclines.pm
@@ -1,8 +1,20 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use 5.010;
+use Getopt::Long;
 use FindBin qw( $Bin );
 use lib "$Bin/lib";
 use Games::ElectricLines;
-Games::ElectricLines->new()->run();
+
+my $lines = 4;
+my $exits = 1;
+my $result = GetOptions(
+    'lines=i' => \$lines,
+    'exits=i' => \$exits,
+);
+die "See README for usage\n" unless $result;
+
+Games::ElectricLines->new(
+    _row_count  => $lines,
+    _exit_count => $exits,
+)->run();
