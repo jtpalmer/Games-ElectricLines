87b48db6027561927a7e7adba27f4721c964822b
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-20 15:15:08 -0400

    Add documentation

--- bin/electric-lines.pl
+++ bin/electric-lines.pl
@@ -26,3 +26,56 @@ Games::ElectricLines->new(
 
 exit;
 
+=pod
+
+=head1 SYNOPSIS
+
+    $ electric-lines.pl
+
+=head1 DESCRIPTION
+
+This script will start the game.
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--lines>=I<nlines>
+
+Set the number of horizontal lines. Defaults to 4.
+
+=item B<--exits>=I<nexits>
+
+Set the number of exits. Defaults to 1.
+
+=back
+
+=head1 RETURN VALUE
+
+Returns 0 for success
+
+=head1 HOW TO PLAY
+
+Guide the plasma balls to the exits (green circles).  Create new routes
+by drawing lines across the horizontal lines using your mouse.
+
+=head1 EXAMPLES
+
+    # Use the default settings.
+    $ electic-lines.pl
+
+    # Specify the number of lines and exits.
+    $ electic-lines.pl --lines=5 --exits=2
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Games::ElectricLines>
+
+=item * L<SDL>
+
+=back
+
+=cut
+
