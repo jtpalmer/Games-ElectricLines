13688330480568e73262c8dcc50eacd53329f20f
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-24 19:57:40 -0400

    Improve darwin SDLPerl check

--- bin/electric-lines.pl
+++ bin/electric-lines.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 BEGIN {
-    if ( $^O eq 'darwin' && $^X ne 'SDLPerl' ) {
+    if ( $^O eq 'darwin' && $^X !~ /SDLPerl$/ ) {
         exec 'SDLPerl', $0, @ARGV or die "Failed to exec SDLPerl: $!";
     }
 }
